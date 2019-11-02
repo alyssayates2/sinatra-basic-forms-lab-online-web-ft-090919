e677a626fe46e7df47c22f88aa21ebf8fecbb4ec
Alyssa Yates <bosslys@icloud.com>
2019-11-02 17:25:21 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -11,7 +11,7 @@ class App < Sinatra::Base
   end
 
   post '/puppy' do
-    @puppy = Puppy.new.create_puppy(params[:puppy])
+    @puppy = Puppy.new.create_puppy(params[:name, :breed, :months_old])
     erb :display_puppy
   end
 end
